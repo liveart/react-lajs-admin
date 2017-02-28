e1056f6909b65b04215383f92988cb8e30dd3725
Oleh S <olehsrh@gmail.com>
2017-02-28 18:34:13 +0200

    Add liveart apis for fonts and colors

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -1,5 +1,28 @@
 'use strict';
-
+const LIVE_ART = 'liveart';
 module.exports = function (app) {
+  app.get('/api/' + LIVE_ART + '/colors', function (req, res) {
+    const loopback = require('loopback');
+    const Color = loopback.getModel('color');
+    const colors = [];
+    Color.find((err, cs) => {
+      cs.map(col => colors.push({name: col.name, value: col.value}));
+      res.send(JSON.stringify({colors: colors}));
+    });
+  });
 
+  app.get('/api/' + LIVE_ART + '/fonts', function (req, res) {
+    const VECTOR_ROOT = '/files/vectors/';
+    const loopback = require('loopback');
+    const Font = loopback.getModel('Font');
+    const fonts = [];
+    Font.find((err, fnts) => {
+      fnts.map(f => fonts.push({
+        name: f.name, fontFamily: f.fontFamily, boldAllowed: f.boldAllowed,
+        italicAllowed: f.italicAllowed, vector: f.vector ? VECTOR_ROOT + f.vector : undefined
+      }));
+
+      res.send(JSON.stringify({fonts: fonts}));
+    });
+  });
 };
