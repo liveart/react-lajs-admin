a02df27caef992af591ead742c9d465df796e706
Oleh S <olehsrh@gmail.com>
2017-03-02 21:13:41 +0200

    Fix user action tests

--- __tests__/actions/user-test.js
+++ __tests__/actions/user-test.js
@@ -2,6 +2,8 @@
 
 import * as U from '../../client/src/actions/user';
 
+const TOKEN = 'USER_ACCESS_TOKEN';
+
 describe('User Actions', () => {
   test('should create an action to get user token', () => {
     const email = 'user@example.com';
@@ -14,7 +16,7 @@ describe('User Actions', () => {
     expect(U.getUserToken(email, password)).toEqual(expectedAction);
   });
 
-  test('should create an action to enable editing', () => {
+  test('should create an action to remove user token', () => {
     const expectedAction = {
       type: U.REMOVE_USER_TOKEN
     };
@@ -23,33 +25,39 @@ describe('User Actions', () => {
 
   test('should create an action to fetch users', () => {
     const expectedAction = {
-      type: U.FETCH_USERS
+      type: U.FETCH_USERS,
+      token: TOKEN
     };
-    expect(U.fetchUsers()).toEqual(expectedAction);
+    expect(U.fetchUsers(TOKEN)).toEqual(expectedAction);
   });
 
   test('should create an action to register user', () => {
     const expectedAction = {
-      type: U.REGISTER_USER
+      type: U.REGISTER_USER,
+      user: {a: 1, b: 2},
+      token: TOKEN
     };
-    expect(U.registerUser()).toEqual(expectedAction);
+    expect(U.registerUser({a: 1, b: 2}, TOKEN)).toEqual(expectedAction);
   });
 
   test('should create an action to edit user', () => {
     const id = 15;
     const expectedAction = {
       type: U.EDIT_USER,
-      id
+      id,
+      newUser: {a: 1, b: 2},
+      token: TOKEN
     };
-    expect(U.editUser(id)).toEqual(expectedAction);
+    expect(U.editUser(id, {a: 1, b: 2}, TOKEN)).toEqual(expectedAction);
   });
 
   test('should create an action to delete user', () => {
     const id = 15;
     const expectedAction = {
       type: U.DELETE_USER,
-      id
+      id,
+      token: TOKEN
     };
-    expect(U.deleteUser(id)).toEqual(expectedAction);
+    expect(U.deleteUser(id, TOKEN)).toEqual(expectedAction);
   });
 });
