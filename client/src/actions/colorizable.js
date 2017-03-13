e6558f2ceb7dbeabb2f17cf8f06601e9d4991675
Oleh S <olehsrh@gmail.com>
2017-03-13 15:16:44 +0200

    Minor change

--- client/src/actions/colorizable.js
+++ client/src/actions/colorizable.js
@@ -1,32 +1,36 @@
 import * as actionTypes from '../actionTypes/colorizable';
 
-export const fetchColorizables = () => {
-    return {
-        type: actionTypes.FETCH_COLORIZABLE
-    };
+export const fetchColorizables = graphicId => {
+  return {
+    type: actionTypes.FETCH_COLORIZABLES,
+    graphicId
+  };
 };
 
-export const createColorizable = (colorizable, token) => {
-    return {
-        type: actionTypes.CREATE_COLORIZABLE,
-        colorizable,
-        token
-    };
+export const createColorizable = (graphicId, colorizable, token) => {
+  return {
+    type: actionTypes.CREATE_COLORIZABLE,
+    colorizable,
+    graphicId,
+    token
+  };
 };
 
-export const editColorizable = (id, colorizable, token) => {
-    return {
-        type: actionTypes.EDIT_COLORIZABLE,
-        id,
-        newColorizable: colorizable,
-        token
-    };
+export const editColorizable = (graphicId, id, colorizable, token) => {
+  return {
+    type: actionTypes.EDIT_COLORIZABLE,
+    graphicId,
+    id,
+    newColorizable: colorizable,
+    token
+  };
 };
 
-export const deleteColorizable = (id, token) => {
-    return {
-        type: actionTypes.DELETE_COLORIZABLE,
-        id,
-        token
-    };
+export const deleteColorizable = (graphicId, id, token) => {
+  return {
+    type: actionTypes.DELETE_COLORIZABLE,
+    graphicId,
+    id,
+    token
+  };
 };
