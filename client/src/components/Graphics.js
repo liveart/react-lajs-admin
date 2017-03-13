964bf7f9c3188b8b1fedc6c3e28eac09a7d5f41e
Oleh S <olehsrh@gmail.com>
2017-03-13 11:28:54 +0200

    Add ColorizableElements model, related table component

--- client/src/components/Graphics.js
+++ client/src/components/Graphics.js
@@ -27,6 +27,11 @@ export default class GraphicsComponent extends Component {
     token: PropTypes.string
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {colorizableNum: 0}
+  }
+
   handleSelectedObjectChange = (propertyName, event) => {
     this.props.setEditingObjectProperty(propertyName, event.target.value);
   };
@@ -42,6 +47,56 @@ export default class GraphicsComponent extends Component {
   handleThumbUpload = file => {
     this.props.uploadGraphicThumb(file);
   };
+
+  handleColorizableRowChange = (prop, i, objectProp, e) => {
+
+  };
+
+  renderColorizableRow = () => {
+    let rows = '';
+    for (let i = 0; i < this.state.colorizableNum; ++i) {
+      rows += <td><input type='text' className='form-control'
+                         value={(this.props.objectHolder['colorizableElements'])[i]['name']}
+                         onChange={e => this.handleColorizableRowChange('colorizableElements', i, 'name', e)}/></td>;
+      rows += <td><input type='text' className='form-control'
+                         value={(this.props.objectHolder['colorizableElements'])[i]['id']}
+                         onChange={e => this.handleColorizableRowChange('colorizableElements', i, 'id', e)}/></td>;
+    }
+    return rows.length ? rows : null
+  };
+
+  changeColorizableRowsNum = inc => {
+    if (inc) {
+      this.setState({...this.state, colorizableNum: this.state.colorizableNum + 1});
+    } else {
+      if (this.state.colorizableNum > 0) {
+        this.setState({...this.state, colorizableNum: this.state.colorizableNum - 1});
+      }
+    }
+  };
+
+  renderColorizableTable = () => (
+    <div>
+      <table className='table table-bordered'>
+        <thead>
+        <tr key='trhead'>
+          <th>name</th>
+          <th>id</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          {this.renderColorizableRow()}
+        </tr>
+        </tbody>
+      </table>
+      <button type='button'
+              className='btn btn-default pull-right'
+              onClick={() => this.changeColorizableRowsNum(true)}
+      >Add new element
+      </button>
+    </div>
+  );
 
   render() {
     return (
@@ -103,6 +158,9 @@ export default class GraphicsComponent extends Component {
                   <option value={false}>No</option>
                   <option value={true}>Yes</option>
                 </select>
+              },
+              colorizableElements: {
+                elem: this.renderColorizableTable()
               }
             }
             }
