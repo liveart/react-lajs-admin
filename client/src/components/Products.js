b2d201d612f701bbc877f2b0fd9f1530fbc69eaa
Oleh S <olehsrh@gmail.com>
2017-04-07 17:18:08 +0300

    Fix lint

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -168,7 +168,6 @@ export default class ProductsComponent extends Component {
       ((((colorizables[fInd])[sArrName])[sInd])[propName]) = event.target.value;
     }
     this.props.setEditingObjectProperty(fArrName, [...colorizables]);
-    console.log(colorizables);
   };
 
   handleSelectedObjectChange = (propertyName, event) => {
@@ -866,12 +865,12 @@ export default class ProductsComponent extends Component {
                                     <div className='input-group-btn'>
                                       <a href={this.getFileUrl(this.getLocationsInputValue('image'))}
                                          className='btn btn-default btn-sm'>{(() => {
-                                        let url = this.getFileUrl(this.getLocationsInputValue('image'));
-                                        if (url.length > 8) {
-                                          url = '...' + url.substr(url.length - 8);
-                                        }
-                                        return url;
-                                      })()
+                                           let url = this.getFileUrl(this.getLocationsInputValue('image'));
+                                           if (url.length > 8) {
+                                             url = '...' + url.substr(url.length - 8);
+                                           }
+                                           return url;
+                                         })()
                                       }</a>
                                     </div> : null
                                   }
@@ -889,12 +888,12 @@ export default class ProductsComponent extends Component {
                                     <div className='input-group-btn'>
                                       <a href={this.getFileUrl(this.getLocationsInputValue('mask'))}
                                          className='btn btn-default btn-sm'>{(() => {
-                                        let url = this.getFileUrl(this.getLocationsInputValue('mask'));
-                                        if (url.length > 8) {
-                                          url = '...' + url.substr(url.length - 8);
-                                        }
-                                        return url;
-                                      })()
+                                           let url = this.getFileUrl(this.getLocationsInputValue('mask'));
+                                           if (url.length > 8) {
+                                             url = '...' + url.substr(url.length - 8);
+                                           }
+                                           return url;
+                                         })()
                                       }</a>
                                     </div> : null
                                   }
@@ -912,12 +911,12 @@ export default class ProductsComponent extends Component {
                                     <div className='input-group-btn'>
                                       <a href={this.getFileUrl(this.getLocationsInputValue('overlayInfo'))}
                                          className='btn btn-default btn-sm'>{(() => {
-                                        let url = this.getFileUrl(this.getLocationsInputValue('overlayInfo'));
-                                        if (url.length > 8) {
-                                          url = '...' + url.substr(url.length - 8);
-                                        }
-                                        return url;
-                                      })()
+                                           let url = this.getFileUrl(this.getLocationsInputValue('overlayInfo'));
+                                           if (url.length > 8) {
+                                             url = '...' + url.substr(url.length - 8);
+                                           }
+                                           return url;
+                                         })()
                                       }</a>
                                     </div> : null
                                   }
