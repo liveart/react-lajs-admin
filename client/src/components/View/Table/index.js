15832f3a513c8960aaf80d7236d040cf9135e48c
Oleh S <olehsrh@gmail.com>
2017-05-05 10:17:28 +0300

    Minor changes

--- client/src/components/View/Table/index.js
+++ client/src/components/View/Table/index.js
@@ -1,117 +1,26 @@
 import React, {Component} from 'react';
-import {capitalizeFirstLetter} from '../../utils';
-import {STATUS_DEFAULT} from '../../definitions';
-import {includes, forEach, keys} from 'lodash';
+import {STATUS_DEFAULT} from '../../../definitions';
+import SortRow from './SortRow';
+import HeadingCell from './HeadingCell';
+import Helper from './Helper';
+import Row from './Row';
+import keys from 'lodash/keys';
 
 export default class Table extends Component {
 
-  renderTableHeadings = () =>
-    keys(this.props.objectSample).map(key => {
-      if (this.props.objectSample[key].showInTable === false) {
-        return null;
-      }
-      return <th key={key}>{this.props.objectSample[key].header ?
-        this.props.objectSample[key].header : capitalizeFirstLetter(key)}</th>;
-    });
+  constructor(props) {
+    super(props);
+    this.helper = new Helper(this.props.data, this.props.objectSample,
+      this.props.objectHolder, this.props.sortComparators);
+  }
 
-  updateObject = (propertyName, event) =>
-    this.props.setEditingObjectProperty(propertyName, event.target.value);
-
-  renderTableSortRow = () => {
-    if (!this.props.sortingSupport || this.props.data.length === 0) {
-      return null;
-    }
-
-    return (
-      <tr>
-        {keys(this.props.objectSample).map(key => {
-          if (this.props.objectSample[key].showInTable === false) {
-            return null;
-          }
-
-          if (this.props.representations && this.props.representations.hasOwnProperty(key)) {
-            if (this.props.objectSample[key].sortable === false) {
-              return <td key={key}/>;
-            } else if (this.props.representations[key].sortElem) {
-              return <td key={key}>{this.props.representations[key].sortElem}</td>;
-            }
-          }
-
-          return <td key={key}><input type='text' className='form-control'
-                                      value={this.props.objectHolder[key]}
-                                      onChange={e => this.updateObject(key, e)}/>
-          </td>;
-        })}
-      </tr>
-    );
-  };
-
-  sortRows = () => {
-    const rows = [];
-    forEach(this.props.data, d => {
-      let add = true;
-
-      keys(this.props.objectSample).map(key => {
-        if (!add) {
-          return;
-        }
-        if (this.props.objectSample[key].showInTable === false) {
-          add = true;
-        } else if (typeof this.props.objectHolder[key] !== 'object') {
-          if (typeof d[key] === 'undefined') {
-            add = this.props.objectHolder[key] === '';
-          } else if (typeof d[key] === 'boolean') {
-            add = true;
-          } else if (this.props.sortComparators && this.props.sortComparators.hasOwnProperty(key)) {
-            add = this.props.sortComparators[key](String(d[key]),
-              String(this.props.objectHolder[key]));
-          } else if (!includes(d[key], this.props.objectHolder[key])) {
-            add = false;
-          }
-        }
-      });
-      if (add) {
-        rows.push(d);
-      }
-    });
-    return rows;
-  };
+  updateObject = (propertyName, event) => this.props.setEditingObjectProperty(propertyName, event.target.value);
 
   handleEdit = object => {
     if (this.props.status === STATUS_DEFAULT) {
       this.props.enableEditing(this.props.objectSample);
       this.props.selectRow(object);
     }
-  };
-
-  renderTableData = () => {
-    if (!this.props.data.length) {
-      return null;
-    }
-    const rows = this.sortRows();
-    return rows.map(item => {
-      return (
-        <tr key={item.id} onClick={() => this.handleEdit(item)}>
-          {
-            Object.getOwnPropertyNames(this.props.objectSample).map(prop => {
-              if (this.props.objectSample[prop].showInTable === false) {
-                return null;
-              }
-
-              if (this.props.representations && this.props.representations.hasOwnProperty(prop)) {
-                return <td key={String(item.id + prop)}>{this.props.representations[prop].getElem(item[prop])}</td>;
-              }
-
-              if (typeof item[prop] === 'object') {
-                return <td key={String(item.id + prop)}/>;
-              }
-
-              return <td key={String(item.id + prop)}>{item[prop]}</td>;
-            })
-          }
-        </tr>
-      );
-    });
   };
 
   render() {
@@ -121,12 +30,22 @@ export default class Table extends Component {
           <table className='table no-margin table-hover table-bordered'>
             <thead>
             <tr>
-              {this.renderTableHeadings()}
+              {keys(this.props.objectSample).map(key => <HeadingCell key={key}
+                                                                     property={key}
+                                                                     objectSample={this.props.objectSample}/>)}
             </tr>
             </thead>
             <tbody>
-            {this.renderTableSortRow()}
-            {this.renderTableData()}
+            <SortRow objectSample={this.props.objectSample}
+                     objectHolder={this.props.objectHolder}
+                     count={this.props.data.length}
+                     representations={this.props.representations}
+                     sortingSupport={this.props.sortingSupport}/>
+            {this.helper.sortRows().map(item => <Row key={item.id}
+                                                     objectSample={this.props.objectSample}
+                                                     handleEdit={this.handleEdit}
+                                                     representations={this.props.representations}
+                                                     item={item}/>)}
             </tbody>
           </table>
         </tb>
